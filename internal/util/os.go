780f32608c03e6b8d97422aea62379f0312369b8
Remuda Agent <remuda-agent@users.noreply.github.com>
2026-06-10 00:54:46 +0000

    chore: remove dead GOOS test helper (km-2vpl)

--- internal/util/os.go
+++ internal/util/os.go
@@ -10,13 +10,3 @@ var runtimeGOOS = runtime.GOOS
 func CurrentGOOS() string {
 	return runtimeGOOS
 }
-
-// SetGOOSForTest overrides the runtime-reported GOOS until the returned
-// cleanup function is called. Intended for use within tests only.
-func SetGOOSForTest(goos string) func() {
-	previous := runtimeGOOS
-	runtimeGOOS = goos
-	return func() {
-		runtimeGOOS = previous
-	}
-}
